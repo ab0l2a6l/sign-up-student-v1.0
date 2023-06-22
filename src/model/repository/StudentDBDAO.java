eaebe733b2e4814c9a277ab19c3edd0e5336a157
abolfazl <a.b.f.m.98@gmail.com>
2023-06-22 12:15:39 +0430

    sakht connection va disconnect kardan

--- src/model/repository/StudentDBDAO.java
+++ src/model/repository/StudentDBDAO.java
@@ -2,7 +2,43 @@ package model.repository;
 
 import model.entity.Student;
 
-public class StudentDBDAO implements StudentDBDAORead , StudentDBDAOWrite{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class StudentDBDAO implements StudentDBDAORead, StudentDBDAOWrite {
+
+    private String url = "jdbc:mysql://localhost:3306/firstdb";
+    private String username = "root";
+    private String password = "Am311865186";
+
+    private String query;
+
+    private Connection connection;
+    private Statement statement;
+
+    public StudentDBDAO() {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+            System.out.println("connected : " + connection);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public  void close() {
+        try {
+            connection.close();
+            statement.close();
+            System.out.println("disconnected");
+
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     @Override
     public Student findById(String id) throws Exception {
         return null;
@@ -27,7 +63,6 @@ public class StudentDBDAO implements StudentDBDAORead , StudentDBDAOWrite{
     public void edit(Student student) throws Exception {
 
     }
-
 
 
 }
